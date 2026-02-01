2cb3ca4aeb1e94aacc34040fe261e4c3ff673608
Earl Paquibol <earl.iii.1998@gmail.com>
2026-02-01 18:19:32 +0800

    cicd deploy to aws

--- .github/actions/deploy-s3-js/main.js
+++ .github/actions/deploy-s3-js/main.js
@@ -1,9 +1,22 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run () {
-    core.notice("Hello world from my custom js action")
+    // Get input
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // Upload to AWS
+    // Looks automatically for env for authentication when using aws sync
+    // Env can be found in the workflow
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+
+    // Output the website's url
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run()
